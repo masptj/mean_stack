c25447e7819d0829835326eac5163fee46142bf3
Marcelino Pinheiro <masptj@gmail.com>
2015-09-08 12:18:51 -0300

    Feito ate 2.5

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var http = require ('http');
-http.createServer(function (req, res)
-    {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Sou um servidor criado pelo Node.js!\n');
-    }).listen(3000, '127.0.0.1');
+var http = require('http');
+
+http.createServer(function (req, res){
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Sou um servidor Criado pelo Node.js\n');
+}).listen(3000, '127.0.0.1');
