81f75183312279579bf643c8c4294a967baeba1c
Marcelino Pinheiro <masptj@gmail.com>
2015-09-15 15:38:17 -0300

    Concluido capitulo 4

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -6,4 +6,40 @@ angular.module('contatooh').controller('ContatosController',
       $scope.incrementa = function(){
         $scope.total++;
       }
-    });
+
+      $scope.contatos = [
+        {
+          "_id": 1,
+          "nome": "Contato Angular 1",
+          "email": "cont1@empresa.com.br"
+        },
+        {
+          "_id": 2,
+          "nome": "Contato Angular 2",
+          "email": "cont2@empresa.com.br"
+        },
+        {
+          "_id": 3,
+          "nome": "Contato Angular 3",
+          "email": "cont3@empresa.com.br"
+        },
+        {
+          "_id": 4,
+          "nome": "Contato Angular 4",
+          "email": "cont4@empresa.com.br"
+        },
+        {
+          "_id": 5,
+          "nome": "Contato Angular 5",
+          "email": "cont5@empresa.com.br"
+        },
+        {
+          "_id": 6,
+          "nome": "Marcelino",
+          "email": "masp@gcarta.com.br"
+        }
+      ];
+
+    $scope.filtro = "";
+
+});
